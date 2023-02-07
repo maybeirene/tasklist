2ac77007dbaf3c91cace11c99353d9daee49b09e
maybeirene <79255144+maybeirene@users.noreply.github.com>
2023-02-07 20:46:15 +0100

    components template and add task function

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import {useState} from 'react'
+import './App.css';
+import AddTask from './AddTask';
+import TaskList from './TaskList.jsx';
+import EmptyTasks from './EmptyTasks';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+// console.log(tasks)
+  const addTask = (task) => {
+    let newTaskList = tasks.concat(task)
+    setTasks(newTaskList)
+  }
+
+  const deleteTask = (index) => {
+
+    console.log('deletetask')
+  }
+
+  const cleanAllTasks = () => {
+    setTasks([])
+  }
+
+  const doneTask = (index) => {
+    console.log('donetask')
+  }
+
+  return (
+    <div className="App">    
+      
+      <AddTask handleAddTask={addTask}/>
+      {
+        tasks.length > 0 ? 
+        <TaskList taskListData={tasks} handleDoneTask={doneTask} handleDeleteTask={deleteTask}/> 
+        :
+        <EmptyTasks/>
+      }
+      
+      <button className="clean-all-task-button" onClick={cleanAllTasks}>Clean all</button>
+    </div>
+  );
+}
+
+export default App;
